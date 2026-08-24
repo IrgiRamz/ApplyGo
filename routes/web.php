fe60d90ef3b98ce5abf18dfd3bbc00d5b78a91a5
IrgiRamz <irgirama01@gmail.com>
2026-08-25 00:13:18 +0700

    update route register

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/login');
+Route::redirect('/register', '/login');
+
 
 // =============================================
 // Auth Routes (Guest)
@@ -19,7 +21,7 @@ Route::redirect('/', '/login');
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    // Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
 });
 
